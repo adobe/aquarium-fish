7dddac74da7dbd2d2adb835e4c3fff5879b69ece
Sergei Parshev <parshev@adobe.com>
2022-04-20 14:52:13 -0700

    OSAB-1230 Added copyright to go headers

--- lib/openapi/openapi.go
+++ lib/openapi/openapi.go
@@ -1,3 +1,15 @@
+/**
+ * Copyright 2021 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
 //go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen@v1.8.1 -config types.cfg.yaml ../../docs/openapi.yaml
 //go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen@v1.8.1 -config meta_v1.cfg.yaml ../../docs/openapi.yaml
 //go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen@v1.8.1 -config api_v1.cfg.yaml ../../docs/openapi.yaml
